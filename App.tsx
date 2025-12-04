0941bf9d22823ce92ff1cc0ca01cc3da81c8be82
paimzh1125-lgtm <pai.mzh1125@gmail.com>
2025-12-04 12:04:43 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 // Pages
-import Home from './pages/Home';
+import Home from './pages/index';
 import About from './pages/About';
 import Products from './pages/Products';
 import News from './pages/News';
