cd1d706c8aed9f61d4d133acd16bed8e46927eb7
pai Ma <pai.mzh1125@gmail.com>
2026-01-22 14:10:26 +0800

    96

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
+import { useLocation } from 'react-router-dom';
 
 interface SEOProps {
   title?: string;
@@ -9,9 +10,13 @@ interface SEOProps {
 
 const SEO: React.FC<SEOProps> = ({ title, description }) => {
   const { t } = useTranslation();
+  const location = useLocation();
   
   const effectiveTitle = title ? `${title} | TopsLife (Suzhou Tops Life)` : t('home.metaTitle');
   const effectiveDesc = description || t('home.metaDesc');
+
+  // Ensure we strip query params (?...) to avoid duplicate content issues
+  const canonicalUrl = `https://${window.location.hostname}${location.pathname}`;
 
   return (
     <Helmet>
@@ -21,6 +26,7 @@ const SEO: React.FC<SEOProps> = ({ title, description }) => {
       <meta property="og:description" content={effectiveDesc} />
       <meta name="twitter:title" content={effectiveTitle} />
       <meta name="twitter:description" content={effectiveDesc} />
+      <link rel="canonical" href={canonicalUrl} />
     </Helmet>
   );
 };
